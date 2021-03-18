69196b9d1e5e6029bb725b8c8f2e58cf1ca968e3
vinodh1988 <k.c.vinodh@hotmail.com>
2021-03-18 10:32:04 +0530

    post-construct-commit

--- src/com/main/MainSpring.java
+++ src/com/main/MainSpring.java
@@ -1,15 +1,13 @@
 package com.main;
 
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.web.servlet.i18n.AcceptHeaderLocaleResolver;
 
 import com.config.JavaConfig;
 
 public class MainSpring {
    public static void main(String[] args) {
-	 ApplicationContext ac=new AnnotationConfigApplicationContext(JavaConfig.class);
-	
+	 AnnotationConfigApplicationContext ac=new AnnotationConfigApplicationContext(JavaConfig.class);
+	  ac.close(); 
 	
 	/* com.system.Process p=(com.system.Process)ac.getBean("back");
 	 p.task();
--- src/com/system/BigThread.java
+++ src/com/system/BigThread.java
@@ -1,17 +1,48 @@
 package com.system;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
 import org.springframework.stereotype.Component;
 
+
 @Component("bt")
-public class BigThread implements SubTask {
+public class BigThread implements SubTask,InitializingBean,DisposableBean{
 
 	{
 		System.out.println("BT Thread is instantiated");
+	}
+	
+	BigThread(){
+		System.out.println("Big Thread Constructor");
+	}
+	
+	@PostConstruct
+	public void init() {
+	  System.out.println("Big Thread Post Constructor logic");	
 	}
 	@Override
 	public void subTask() {
 		// TODO Auto-generated method stub
        System.out.println("Big Thread Started");
+	}
+
+	@Override
+	public void destroy() throws Exception {
+		// TODO Auto-generated method stub
+		System.out.println("Big Thread Disposed");
+	}
+    
+	@PreDestroy
+	public void predestroy() {
+	  System.out.println("Big Thread PreDestroy logic");	
+	}
+	@Override
+	public void afterPropertiesSet() throws Exception {
+		// TODO Auto-generated method stub
+		System.out.println("Big Thread Initialized");
 	}
 
 }
